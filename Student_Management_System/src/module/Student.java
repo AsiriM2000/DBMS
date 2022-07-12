b06210a8f0e7d66a9c1abf6feaa2bd864272218e
AsiriM2000 <asiriambagahawaththa@gmai.com>
2022-07-12 10:27:32 +0530

    Insert Data Database

--- Student_Management_System/src/module/Student.java
+++ Student_Management_System/src/module/Student.java
@@ -1,4 +1,71 @@
 package module;
 
 public class Student {
+    private String student_id;
+    private String student_name;
+    private String email;
+    private String address;
+    private String nic;
+
+    public Student() {
+    }
+
+    public Student(String student_id, String student_name, String email, String address, String nic) {
+        this.student_id = student_id;
+        this.student_name = student_name;
+        this.email = email;
+        this.address = address;
+        this.nic = nic;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(String student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "student_id='" + student_id + '\'' +
+                ", student_name='" + student_name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }
