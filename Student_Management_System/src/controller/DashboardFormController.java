0964784fbddcf0ee6665df50f8190d2ec078f28c
AsiriM2000 <asiriambagahawaththa@gmai.com>
2022-07-12 10:38:28 +0530

    Immpliment Dashboard Options

---
+++ Student_Management_System/src/controller/DashboardFormController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardFormController {
+
+    public AnchorPane dashboard;
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadAllOnAction(ActionEvent actionEvent) {
+    }
+}
